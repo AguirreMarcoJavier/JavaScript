5d6fff290f3d3b69fcaf454f7434ec4ed24e8bc5
Marco <103866262+AguirreMarcoJavier@users.noreply.github.com>
2022-05-28 22:50:35 -0300

    Borramos Prueba y agregamos La actividad 1

---
+++ JS/mi_archivo_js.js
@@ -0,0 +1,5 @@
+var numero = prompt("Ingrese un numero por favor: ");
+alert ('Muy bien, se Mostrará hola ' + numero + " de veces en la consola")
+for (let i = 0; i < numero; i++) {
+  console.log ('Hola');
+}
